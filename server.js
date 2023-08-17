e4c14f9f226a166341502c7c40cef354d989a968
denis-marul <marul2na3@gmail.com>
2023-08-18 00:57:41 +0300

    neew
    neew

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  'mongodb+srv://Dendi:Wpt4j0H2RmVYEoTc@cluster0.i7uvdld.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Database connection successful');
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
